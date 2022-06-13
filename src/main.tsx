b2ec5b4496939b00f63cf5e816653a291afe4f65
Jaehyeok <milkboy2564@naver.com>
2022-06-13 22:13:57 +0900

    Test: Remote Test

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './styles/global-style';
-import { theme } from './styles/theme';
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global-style";
+import { theme } from "./styles/theme";
 
-import App from './App';
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Test
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
