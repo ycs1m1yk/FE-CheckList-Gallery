368900692fada67b0118fd5ea6ff77b889e91200
ycs1m1yk <satoly4@gmail.com>
2022-06-18 02:57:17 +0900

    Feat: Gallery 컴포넌트 구조화
    + GalleryContainer 추가
    + TagList, Tags 컴포넌트를 SideBar로 통합

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,26 +1,37 @@
-import { useState } from "react";
-import Slider from "../common/Slider";
-import Loader from "../common/Loader";
-const Gallery = () => {
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Slider from '../common/Slider';
+import Loader from '../common/Loader';
+import SideBar from './SideBar';
+
+const GalleryContainer = styled.div`
+  width: 768px;
+  margin-left: auto;
+  margin-right: auto;
+
+  @media ${(props) => props.theme.devices.desktop} {
+    width: 100%;
+  };
+`;
+
+function Gallery() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const handleLoad = () => {
-    console.log("Content Load Finish");
+    console.log('Content Load Finish');
     setIsLoading(false);
   };
 
-  window.addEventListener("load", handleLoad);
+  window.addEventListener('load', handleLoad);
 
   return isLoading ? (
     <Loader />
   ) : (
-    <>
-      {/* <Header /> */}
-      {/* <Sidebar /> */}
+    <GalleryContainer>
+      <SideBar />
       <Slider />
-      {/* <Footer /> */}
-    </>
+    </GalleryContainer>
   );
-};
+}
 
 export default Gallery;
