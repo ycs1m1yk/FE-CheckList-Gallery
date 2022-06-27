9384d811447cf29774ace45ba2b4432cbdec57ee
Jaehyeok <milkboy2564@naver.com>
2022-06-27 21:37:17 +0900

    Fix: lint

--- src/lib/DetailViewer.tsx
+++ src/lib/DetailViewer.tsx
@@ -13,28 +13,28 @@ const Container = styled.div`
     height: 500px;
     overflow: visible;
     resize: both;
-`
+`;
 
 export function DetailViewer({ files }:{files: ICodeProps[]}) {
   const [defaultFile, setDefaultFile] = useState<string>();
   useEffect(() => {
-    if(files){
-      files.forEach(file=>{
-        if(file.fileName==='Default.jsx'){
+    if (files) {
+      files.forEach((file) => {
+        if (file.fileName === 'Default.jsx') {
           const fileEndPoint = file.fileUrl
-          .split('/')
-          .slice(-2)
-          .join('/')
-          .slice(0,-4)
-          console.log(fileEndPoint)
-          setDefaultFile(fileEndPoint)
+            .split('/')
+            .slice(-2)
+            .join('/')
+            .slice(0, -4);
+          console.log(fileEndPoint);
+          setDefaultFile(fileEndPoint);
         }
-      })
+      });
     }
   }, []);
   return (
     <Container>
-      {defaultFile && <FrameViewer scrolling="no" maginwidth="0" marginheight="0" frameborder="0" src={import.meta.env.VITE_API_URL+'view'+'/'+defaultFile} />}
+      {defaultFile && <FrameViewer scrolling="no" maginwidth="0" marginheight="0" frameborder="0" src={`${import.meta.env.VITE_API_URL}view` + `/${defaultFile}`} />}
     </Container>
   );
 }
