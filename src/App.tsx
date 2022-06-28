6e084cc17ea32b4e7f085399eb1ca6fbc2bf54fe
Jaehyeok <milkboy2564@naver.com>
2022-06-28 16:06:49 +0900

    Feat: Main Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
-import React, {
-  createContext, useEffect, useState,
-} from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import Home from './components/home/Home';
 import Gallery from './components/gallery/Gallery';
 import Detail from './components/gallery/Detail';
 import Publish from './components/publish/Publish';
 import AuthCallback from './components/auth/AuthCallback';
-
-export const LoginContext = createContext(null);
+import Layout from './components/common/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/auth/callback" element={<AuthCallback />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/gallery/:postId" element={<Detail />} />
-        <Route path="/publish" element={<Publish />} />
+        <Route element={<Layout />}>
+          <Route path="/auth/callback" element={<AuthCallback />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/gallery/:postId" element={<Detail />} />
+          <Route path="/publish" element={<Publish />} />
+        </Route>
       </Routes>
       <Footer />
 
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  background-image: url("https://i.gifer.com/76YS.gif");
+  margin: 0;
+  padding: 0;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+`;
+
+const TitleContainer = styled.div`
+  color: white;
+`;
+
+const MainTitle = styled.div`
+  font-size: 3rem;
+  margin-bottom: 1rem;
+`;
+
+const SubTitle = styled.div`
+  font-size: 1.5rem;
+  text-align: center;
+`;
+
+const EnterLink = styled(Link)`
+  position: fixed;
+  bottom: 30%;
+  left: 45%;
+  color: white;
+  margin-top: 1rem;
+  text-decoration: none;
+  font-size: 1.5rem;
+  border: 1px solid white;
+  padding: 0.5rem;
+  border-radius: 1rem;
+  &: hover {
+    background-color: white;
+    color: black;
+  }
+`;
 
 export default function Home() {
-  return <div />;
+  return (
+    <Container>
+      <TitleContainer>
+        <MainTitle>프론트엔드 개발자 기본 소양 전시회</MainTitle>
+        <SubTitle>To be a great Front-end Developer</SubTitle>
+        <EnterLink to="/gallery">들어가기</EnterLink>
+      </TitleContainer>
+    </Container>
+  );
 }
